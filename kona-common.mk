43c8fbe3015fcaf0c14aa747864836c81b0e8efe
Buff99 <buffbuffster@gmail.com>
2017-12-22 23:57:33 +0000

    kona-common: remmove false rootdir init.smdk4x12.usb.rc

--- kona-common.mk
+++ kona-common.mk
@@ -36,7 +36,6 @@ TARGET_HAS_CAM_FLASH := false
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.smdk4x12:root/fstab.smdk4x12 \
     $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
-    $(LOCAL_PATH)/rootdir/init.smdk4x12.usb.rc:root/init.smdk4x12.usb.rc \
     $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc \
     $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc
 
